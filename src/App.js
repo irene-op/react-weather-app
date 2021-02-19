2dce827e1c837f30e8fe3a9b4f35aeccce1f78aa
irene-op <72819188+irene-op@users.noreply.github.com>
2021-02-19 12:07:22 +0100

    Bootstrap added

--- src/App.js
+++ src/App.js
@@ -3,13 +3,18 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather app</h1>
-      <footer>
-        This project was coded by Irene Ortega and is{" "}
-        <a href="https://github.com/irene-op/react-weather-app" target="blank">
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather app</h1>
+        <footer>
+          This project was coded by Irene Ortega and is{" "}
+          <a
+            href="https://github.com/irene-op/react-weather-app"
+            target="blank"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
