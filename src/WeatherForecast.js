13d06dcd37c79d5bff84815e44712d8f3b4a8905
irene-op <72819188+irene-op@users.noreply.github.com>
2021-02-22 17:48:39 +0100

    added new WeatherForecastPreview component for cleaner workspace

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import axios from "axios";
 import "./WeatherForecast.css";
+import "./WeatherForecastPreview";
+import WeatherForecastPreview from "./WeatherForecastPreview";
 
 export default function WeatherForecast(props) {
   const [loaded, setLoaded] = useState(false);
@@ -14,95 +16,12 @@ export default function WeatherForecast(props) {
 
   if (loaded) {
     return (
-      <div className="WeatherForecast">
-        <div className="forecast">
-          <div className="row" id="forecast">
-            <div className="col five-days">
-              <strong>10:00</strong> <br />
-              <div className="low-high">
-                {Math.round(forecast.list[0].main.temp_min)}ºC
-                <br />
-                <strong>{Math.round(forecast.list[0].main.temp_max)}ºC</strong>
-                <br />
-              </div>
-              <span className="icons">
-                <img
-                  src="http://openweathermap.org/img/wn/01d@2x.png"
-                  alt="Clear"
-                  id="main-icon"
-                />
-              </span>
-            </div>
-            <div className="col five-days">
-              <strong>15:00</strong> <br />
-              <div className="low-high">
-                15°C
-                <br />
-                <strong>22°C</strong>
-                <br />
-              </div>
-              <span className="icons">
-                <img
-                  src="http://openweathermap.org/img/wn/01d@2x.png"
-                  alt="Clear"
-                  id="main-icon"
-                />
-              </span>
-            </div>
-            <div className="col five-days">
-              <strong>18:00</strong> <br />
-              <div className="low-high">
-                15°C
-                <br />
-                <strong>22°C</strong>
-                <br />
-              </div>
-              <span className="icons">
-                <img
-                  src="http://openweathermap.org/img/wn/01d@2x.png"
-                  alt="Clear"
-                  id="main-icon"
-                />
-              </span>
-            </div>
-            <div className="col five-days">
-              <strong>21:00</strong> <br />
-              <div className="low-high">
-                15°C
-                <br />
-                <strong>22°C</strong>
-                <br />
-              </div>
-              <span className="icons">
-                <img
-                  src="http://openweathermap.org/img/wn/01d@2x.png"
-                  alt="Clear"
-                  id="main-icon"
-                />
-              </span>
-            </div>
-            <div className="col five-days">
-              <strong>00:00</strong> <br />
-              <div className="low-high">
-                15°C
-                <br />
-                <strong>22°C</strong>
-                <br />
-              </div>
-              <span className="icons">
-                <img
-                  src="http://openweathermap.org/img/wn/01d@2x.png"
-                  alt="Clear"
-                  id="main-icon"
-                />
-              </span>
-            </div>
-          </div>
-        </div>
-        {/* 10:00
-        {Math.round(forecast.list[0].main.temp_max)}ºC
-        {Math.round(forecast.list[0].main.temp_min)}ºC */}
-        {/* <img src=`http://openweathermap.org/img/wn/${iconCode}@2x.png`/> */}
+      <div className="WeatherForecast row">
+        <WeatherForecastPreview data={forecast.list[0]} />
+        <WeatherForecastPreview data={forecast.list[1]} />
+        <WeatherForecastPreview data={forecast.list[2]} />
+        <WeatherForecastPreview data={forecast.list[3]} />
+        <WeatherForecastPreview data={forecast.list[4]} />
       </div>
     );
   } else {
@@ -111,4 +30,91 @@ export default function WeatherForecast(props) {
     axios.get(apiUrl).then(handleForecastResponse);
     return null;
   }
+}
+
+{
+  /* <div className="forecast">
+  <div className="row" id="forecast">
+    <div className="col five-days">
+      <strong>10:00</strong> <br />
+      <div className="low-high">
+        {Math.round(forecast.list[0].main.temp_min)}ºC
+        <br />
+        <strong>{Math.round(forecast.list[0].main.temp_max)}ºC</strong>
+        <br />
+      </div>
+      <span className="icons">
+        <img
+          src="http://openweathermap.org/img/wn/01d@2x.png"
+          alt="Clear"
+          id="main-icon"
+        />
+      </span>
+    </div>
+    <div className="col five-days">
+      <strong>15:00</strong> <br />
+      <div className="low-high">
+        15°C
+        <br />
+        <strong>22°C</strong>
+        <br />
+      </div>
+      <span className="icons">
+        <img
+          src="http://openweathermap.org/img/wn/01d@2x.png"
+          alt="Clear"
+          id="main-icon"
+        />
+      </span>
+    </div>
+    <div className="col five-days">
+      <strong>18:00</strong> <br />
+      <div className="low-high">
+        15°C
+        <br />
+        <strong>22°C</strong>
+        <br />
+      </div>
+      <span className="icons">
+        <img
+          src="http://openweathermap.org/img/wn/01d@2x.png"
+          alt="Clear"
+          id="main-icon"
+        />
+      </span>
+    </div>
+    <div className="col five-days">
+      <strong>21:00</strong> <br />
+      <div className="low-high">
+        15°C
+        <br />
+        <strong>22°C</strong>
+        <br />
+      </div>
+      <span className="icons">
+        <img
+          src="http://openweathermap.org/img/wn/01d@2x.png"
+          alt="Clear"
+          id="main-icon"
+        />
+      </span>
+    </div>
+    <div className="col five-days">
+      <strong>00:00</strong> <br />
+      <div className="low-high">
+        15°C
+        <br />
+        <strong>22°C</strong>
+        <br />
+      </div>
+      <span className="icons">
+        <img
+          src="http://openweathermap.org/img/wn/01d@2x.png"
+          alt="Clear"
+          id="main-icon"
+        />
+      </span>
+    </div>
+  </div>
+</div>; */
 }
---
+++ src/WeatherForecastPreview.js
@@ -0,0 +1,36 @@
+import React from "react";
+
+export default function WeatherForecastPreview(props) {
+  function hours() {
+    let date = new Date(props.data.dt * 1000);
+    let hours = date.getHours();
+
+    return `${hours}:00`;
+  }
+
+  function minTemperature() {
+    let minTemperature = Math.round(props.main.temp_min);
+    return `${minTemperature}ºC`;
+  }
+  function maxTemperature() {
+    let maxTemperature = Math.round(props.main.temp_max);
+    return `${maxTemperature}ºC`;
+  }
+
+  return (
+    <div className="col five-days">
+      <strong>{hours()}</strong>
+      <br />
+      <div className="low-high">
+        {minTemperature()}ºC
+        <br />
+        <strong>{maxTemperature()}ºC</strong>
+      </div>
+      <img
+        src="http://openweathermap.org/img/wn/01n@2x.png"
+        alt="Clear"
+        id="main-icon"
+      />
+    </div>
+  );
+}
